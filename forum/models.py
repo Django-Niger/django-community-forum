6640ae2fec662a41c858c131857bef7364658488
HAMA Barhamou <hamabarhamou@gmail.com>
2024-05-31 20:06:00 +0100

    Added Post model with tests for creating and associating with discussions and users

--- forum/models.py
+++ forum/models.py
@@ -4,3 +4,12 @@ from django.db import models
 class Discussion(models.Model):
     title = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Post(models.Model):
+    discussion = models.ForeignKey(
+        Discussion, on_delete=models.CASCADE, related_name="posts"
+    )
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- forum/tests.py
+++ forum/tests.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
-from .models import Discussion
+from .models import Discussion, Post
 
 
 class DiscussionModelTests(TestCase):
@@ -8,3 +9,16 @@ class DiscussionModelTests(TestCase):
         discussion = Discussion(title="Example Discussion")
         discussion.save()
         self.assertEqual(discussion.title, "Example Discussion")
+
+
+class PostModelTests(TestCase):
+    def test_creating_post(self):
+        user = User.objects.create_user(username="testuser", password="12345")
+        discussion = Discussion.objects.create(title="Test Discussion")
+        post = Post.objects.create(
+            discussion=discussion, author=user, content="Test content"
+        )
+        post.save()
+        self.assertEqual(post.discussion, discussion)
+        self.assertEqual(post.author, user)
+        self.assertEqual(post.content, "Test content")
